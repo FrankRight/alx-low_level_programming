d65a3d7ce6a690b5657b147fc9344bb26881b327
FrankRight <frankbosire2017@gmail.com>
2022-06-26 22:41:26 +0300

    6-size fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 return (0);
 }
