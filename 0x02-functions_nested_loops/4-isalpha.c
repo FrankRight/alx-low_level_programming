7aa72a4b5221f7b01d2f67176bf4a151e72f5fc2
FrankRight <frankbosire2017@gmail.com>
2022-07-05 21:13:22 +0300

    4 check if character is alphanumeric

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _islower - checks for lower or upper case
- * @c: The character to check if uppercase
+ * _isalpha - checks for alphanumeric characters
+ * @c: The character to check if alphanumeric
  *
  * Return: On success 1 || 0.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-if (islower(c) > 0)
+if (isalpha(c) > 0)
 {
 return (1);
 }
