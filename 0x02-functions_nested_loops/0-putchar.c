d810a5dc6a44b6c2009e7243972112182ad4cbc1
FrankRight <frankbosire2017@gmail.com>
2022-07-05 19:48:04 +0300

    put char print fix bug

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - check the code
@@ -7,6 +7,14 @@
  */
 int main(void)
 {
-printf("_putchar\n");
+char arr[8] = "_putchar";
+int i;
+
+for (i = 0; i < 8; i++)
+{
+_putchar(arr[i]);
+}
+_putchar('\n');
+
 return (0);
 }
