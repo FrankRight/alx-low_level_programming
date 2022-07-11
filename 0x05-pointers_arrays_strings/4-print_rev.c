ef9738b4888614242fb374a298c4bcfef38b1c61
FrankRight <frankbosire2017@gmail.com>
2022-07-11 23:26:46 +0300

    4 print rev betty revise

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,17 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+int len = 0, index;
 
-	while (s[index++])
-		len++;
+while (s[index++])
+{
+len++;
+}
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+for (index = len - 1; index >= 0; index--)
+{
+_putchar(s[index]);
+}
 
-	_putchar('\n');
+_putchar('\n');
 }
