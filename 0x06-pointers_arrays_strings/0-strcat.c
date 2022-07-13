5522910d5f3d90d182e17c83dc3cca45950c99b6
FrankRight <frankbosire2017@gmail.com>
2022-07-13 20:17:08 +0300

    concantinate 2 strings fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,20 +12,7 @@
 
 char *_strcat(char *dest, char *src)
 {
-unsigned int i = 0;
-unsigned int j = 0;
-
-while (*(dest + i) != '\0')
-{
-i++;
-}
-
-while (*(src + j) != '\0')
-{
-*(dest + i) = *(src + j);
-i++;
-j++;
-}
+strcat(dest, src);
 
 return (dest);
 }
